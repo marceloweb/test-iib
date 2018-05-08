f41645844214e348b989d57ced15ee9012129a8b
Marcelo Oliveira <marcelo@marceloweb.info>
2018-05-08 09:51:53 -0300

    - multiple deployment

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ TYPE=$1
 ENV=$2
 APP=$4
 NUMBER=$3
-CONTAINER_EG=$APP$ENV$TYPE
+CONTAINER_EG=$APP$TYPE
 
 die () {
     echo >&2 "$@"
@@ -45,41 +45,47 @@ HTTPS_PORT=$(echo `expr $HTTP_PORT + 1`)
 
 mqsipackagebar -a $APP.bar -w $WORKSPACE -k $APP
 
-BROKER=BRAMIL$TYPE$ENV$NUMBER
-echo "Preparando $BROKER..."
+ENVS=( "DSV" "QA" "HOM" )
 
-EGs=$(mqsilist $BROKER)
+for i in "${ENVS[@]}"
+do
+   ENV=$i
+   BROKER=BRAMIL$TYPE$ENV$NUMBER
+   echo "Preparando $BROKER..."
+
+   EGs=$(mqsilist $BROKER)
         
-if [[ $BROKER = *"$EGs"* ]]
-then
-   echo "Execution grupo $CONTAINER_EG já existe e não será criado."
-else
-   echo "Execution group $CONTAINER_EG não existe e será criado."
-   mqsicreateexecutiongroup $BROKER -e $CONTAINER_EG
-   if [ $? -eq 0 ]
+   if [[ $BROKER = *"$EGs"* ]]
    then
-      echo "Execution group $CONTAINER_EG criado com sucesso!"
+      echo "Execution grupo $CONTAINER_EG já existe e não será criado."
    else
-      echo "Erro na criacao do execution group $CONTAINER_EG"
+      echo "Execution group $CONTAINER_EG não existe e será criado."
+      mqsicreateexecutiongroup $BROKER -e $CONTAINER_EG
+      if [ $? -eq 0 ]
+      then
+         echo "Execution group $CONTAINER_EG criado com sucesso!"
+      else
+         echo "Erro na criacao do execution group $CONTAINER_EG"
+      fi
    fi
-fi
 
-echo "Setando listeners HTTP ($HTTP_PORT)/HTTPS($HTTPS_PORT)..."
-echo $CONTAINER_EG
+   echo "Setando listeners HTTP ($HTTP_PORT)/HTTPS($HTTPS_PORT)..."
+   echo $CONTAINER_EG
 
-mqsichangeproperties $BROKER -e $CONTAINER_EG -o ExecutionGroup -n httpNodesUseEmbeddedListener -v true
-mqsichangeproperties $BROKER -e $CONTAINER_EG -o ExecutionGroup -n soapNodesUseEmbeddedListener -v true
-mqsichangeproperties $BROKER -e $CONTAINER_EG -o HTTPConnector -n explicitlySetPortNumber -v $HTTP_PORT
-mqsichangeproperties $BROKER -e $CONTAINER_EG -o HTTPSConnector -n explicitlySetPortNumber -v $HTTPS_PORT
+   mqsichangeproperties $BROKER -e $CONTAINER_EG -o ExecutionGroup -n httpNodesUseEmbeddedListener -v true
+   mqsichangeproperties $BROKER -e $CONTAINER_EG -o ExecutionGroup -n soapNodesUseEmbeddedListener -v true
+   mqsichangeproperties $BROKER -e $CONTAINER_EG -o HTTPConnector -n explicitlySetPortNumber -v $HTTP_PORT
+   mqsichangeproperties $BROKER -e $CONTAINER_EG -o HTTPSConnector -n explicitlySetPortNumber -v $HTTPS_PORT
 
-echo "Configurando log4j..."
-LOG4J_CONFIG_DIR=/var/mqsi/config/$BROKER/$CONTAINER_EG/shared-classes/log4j.jar
-#mkdir -p $LOG4J_CONFIG_DIR
-#cp log4j.xml $LOG4J_CONFIG_DIR
-echo "Arquivo log4j.xml copiado para $LOG4J_CONFIG_DIR"
-#mqsichangeproperties $BROKER -e $CONTAINER_EG -o ComIbmJVMManager -n jvmSystemProperty -v"-Dlog4j.debug -Dlog4j.configurationFile=file://${LOG4J_CONFIG_DIR}/log4j.xml"
+   echo "Configurando log4j..."
+   LOG4J_CONFIG_DIR=/var/mqsi/config/$BROKER/$CONTAINER_EG/shared-classes/log4j.jar
+   #mkdir -p $LOG4J_CONFIG_DIR
+   #cp log4j.xml $LOG4J_CONFIG_DIR
+   echo "Arquivo log4j.xml copiado para $LOG4J_CONFIG_DIR"
+   #mqsichangeproperties $BROKER -e $CONTAINER_EG -o ComIbmJVMManager -n jvmSystemProperty -v"-Dlog4j.debug -Dlog4j.configurationFile=file://${LOG4J_CONFIG_DIR}/log4j.xml"
 
-echo "Reiniciando EG $CONTAINER_EG"
-mqsireload $BROKER -e $CONTAINER_EG
-echo "Iniciando deploy..."
-mqsideploy $BROKER -e $CONTAINER_EG -a $APP.bar
+   echo "Reiniciando EG $CONTAINER_EG"
+   mqsireload $BROKER -e $CONTAINER_EG
+   echo "Iniciando deploy em $CONTAINER_EG..."
+   mqsideploy $BROKER -e $CONTAINER_EG -a $APP.bar
+done
