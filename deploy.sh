73491b9c91f0210e72bdfabe9412732f2f41437f
Marcelo Oliveira <marcelo@marceloweb.info>
2018-05-09 16:14:13 -0300

    - new rule for version name

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ echo $3 | grep -E -q '^[0-9]+$' || die "O argumento 3 deve ser numérico. $3 nã
 if [ -n ROLLBACK ]
 then
 
-  GIT_HASH=SUBSTRING=${GIT_COMMIT:0:7}
+  GIT_HASH=${GIT_COMMIT:0:7}
   GIT_HASH_PREVIOUS=${GIT_PREVIOUS_COMMIT:0:7}
   echo $GIT_HASH' '$GIT_HASH_PREVIOUS
   APP_VERSION=$APP'-v1.1-'$GIT_HASH
